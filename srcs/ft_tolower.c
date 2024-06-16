90bdaf40145d4e733542b251a0d833da75c4ba05
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-06-16 03:12:56 +0200

    Las funciones staticas solo abarcan al archivo en lass quales estans definidas

--- srcs/ft_tolower.c
+++ srcs/ft_tolower.c
@@ -6,11 +6,16 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:41:05 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/12 19:58:56 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/16 03:08:57 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_isupper(char letter)
+{
+	return (letter >= 'a' && letter <= 'z');
+}
 
 char	*ft_tolower(char *str)
 {
--- srcs/ft_toupper.c
+++ srcs/ft_toupper.c
@@ -6,11 +6,16 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:42:38 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/12 19:59:18 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/16 03:09:35 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_islower(char letter)
+{
+	return (letter >= 'A' && letter <= 'Z');
+}
 
 char	*ft_toupper(char *str)
 {
