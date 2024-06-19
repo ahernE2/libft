125c00c8e8cd9ad123237d8f1b0088b0650a991a
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-06-19 23:36:56 +0200

    Eliminación de función statica strcpy y llamado strlcat (Optimización de código)

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,25 +6,11 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 02:41:28 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/18 23:46:53 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/19 22:37:13 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*ft_strcpy(char *dest, char *src)
-{
-	size_t	index;
-
-	index = 0;
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
-	return (dest);
-}
 
 char	*ft_strdup(char *str)
 {
@@ -32,9 +18,9 @@ char	*ft_strdup(char *str)
 	char	*dup;
 
 	size = ft_strlen(str) + 1;
-	dup = (char *)malloc(size * sizeof(char));
+	dup = (char *)ft_calloc(size, sizeof(char));
 	if (!dup)
 		return (NULL);
-	ft_strcpy(dup, str);
+	ft_strlcpy(dup, str, size);
 	return (dup);
 }
