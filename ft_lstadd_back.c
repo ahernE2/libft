8c407e354848883b14f438313a0ab24e78852f62
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-06-23 17:08:41 +0200

    se han concatendado los if del principio

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 00:38:01 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/23 01:19:17 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/23 16:37:27 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!lst || !new)
-		return ;
-	if (!(*lst))
+	if (!lst || !new || !(*lst))
 		return ;
 	ft_lst_last(*lst)-> next = *new;
 }
