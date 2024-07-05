d7762cf39a221e252f864c9eb53f85c745958952
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-07-05 23:58:44 +0200

    controlando nulls

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 22:46:30 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/23 17:05:46 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/07/05 23:56:56 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	t_size;
 	char	*arr;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	t_size = ft_strlen(s1) + ft_strlen(s2) + 1;
 	arr = (char *)ft_calloc(t_size, sizeof(char));
 	if (!arr)
