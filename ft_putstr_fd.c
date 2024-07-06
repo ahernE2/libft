0be89126181a461e5df341df2be26f738b304233
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-07-06 21:53:16 +0200

    manejo del null

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 20:32:19 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/24 19:03:59 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/07/06 21:52:38 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	while (*s)
 	{
 		ft_putchar_fd(*s, fd);
